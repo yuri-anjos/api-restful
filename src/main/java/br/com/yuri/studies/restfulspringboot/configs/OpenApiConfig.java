0cee7a788a1c4bba1f8e2dd18d51873d7514b156
yuri-anjos <yuri.dos.4njos@gmail.com>
2024-04-09 20:41:21 -0300

    openapi security scheme config

--- src/main/java/br/com/yuri/studies/restfulspringboot/configs/OpenApiConfig.java
+++ src/main/java/br/com/yuri/studies/restfulspringboot/configs/OpenApiConfig.java
@@ -1,5 +1,10 @@
 package br.com.yuri.studies.restfulspringboot.configs;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -7,20 +12,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer", in = SecuritySchemeIn.HEADER)
 public class OpenApiConfig {
+
 	@Bean
 	public OpenAPI customOpenApi() {
-		return new OpenAPI()
-				.info(new Info()
-						.title("Api Restful Spring Boot 3")
-						.version("v1")
-						.description("Api Restful Spring Boot 3")
-						.termsOfService("")
-						.license(
-								new License()
-										.name("")
-										.url("")
-						)
-				);
+		return new OpenAPI().info(new Info()
+				.title("Api Restful Spring Boot 3")
+				.version("v1")
+				.description("Api Restful Spring Boot 3")
+				.termsOfService("")
+				.license(
+						new License()
+								.name("")
+								.url("")
+				)
+		);
 	}
 }
